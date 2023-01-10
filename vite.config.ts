f6a15b06b4a454eca7cba078bb15ff7b91be1fe4
Alejandro Cerezo <alce65@hotmail.es>
2023-01-10 12:59:29 +0100

    Update for prettier single quote configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 /* eslint-disable camelcase */
-import { defineConfig } from "vite";
-import { resolve } from "path";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
 
 // Multi project
 // src
@@ -20,13 +20,13 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        main_ch0: resolve(__dirname, "CH0/index.html"),
-        main_ch1: resolve(__dirname, "CH1.Gents/index.html"),
-        main_ch2: resolve(__dirname, "CH2.Form/index.html"),
-        main_ch3: resolve(__dirname, "CH3.GoT/index.html"),
-        main_ch4l: resolve(__dirname, "CH4.List/index.html"),
-        main_ch4p: resolve(__dirname, "CH4.Phone/index.html"),
+        main: resolve(__dirname, 'index.html'),
+        main_ch0: resolve(__dirname, 'CH0/index.html'),
+        main_ch1: resolve(__dirname, 'CH1.Gents/index.html'),
+        main_ch2: resolve(__dirname, 'CH2.Form/index.html'),
+        main_ch3: resolve(__dirname, 'CH3.GoT/index.html'),
+        main_ch4l: resolve(__dirname, 'CH4.List/index.html'),
+        main_ch4p: resolve(__dirname, 'CH4.Phone/index.html'),
       },
     },
   },
