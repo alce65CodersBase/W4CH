3e0756021721125b26a71c7880fbbfb16fdcbc4d
Alejandro Cerezo <alce65@hotmail.es>
2023-01-10 14:54:04 +0100

    Update previous tests

--- src/CH1.Gents/components/app/app.test.tsx
+++ src/CH1.Gents/components/app/app.test.tsx
@@ -6,8 +6,8 @@ describe('Given the component App', () => {
   describe('When it is rendered', () => {
     test('Then it should be on the screen', () => {
       render(<App />);
-      const header = screen.getByRole('heading', { name: 'CH1 Gents' });
-      expect(header).toBeInTheDocument();
+      const app = screen.getByRole('application');
+      expect(app).toBeInTheDocument();
     });
   });
 });
