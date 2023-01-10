05ea068fb85ee6773978fc1e5c851c6b3c34f0ee
Alejandro Cerezo <alce65@hotmail.es>
2023-01-10 18:32:12 +0100

    Add style css to components and remove s static

--- src/CH1.Gents/components/app/app.tsx
+++ src/CH1.Gents/components/app/app.tsx
@@ -1,11 +1,22 @@
+import { Button } from '../button/button';
+import { Gent } from '../gent/gent';
 import { Header } from '../header/header';
 import { Info } from '../info/info';
 
+import './app.css';
+
 export function App() {
   return (
-    <div className="App container" role="application">
+    <div className="app-container" role="application">
       <Header></Header>
-      <Info></Info>;
+      <Info>
+        <Button></Button>
+      </Info>
+      <main className="main">
+        <ul className="gentlemen">
+          <Gent></Gent>
+        </ul>
+      </main>
     </div>
   );
 }
--- src/CH1.Gents/main_ch1.tsx
+++ src/CH1.Gents/main_ch1.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components/app/app';
 
-// Temp import "./index.css";
+import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
