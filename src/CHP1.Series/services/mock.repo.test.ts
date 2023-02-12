60c5618cc331578aa7c4d8c582be396d5870b8bb
Alejandro Cerezo <alce65@hotmail.es>
2023-02-12 18:45:32 +0100

    Refactor the text for the async load of the data

--- src/CHP1.Series/services/mock.repo.test.ts
+++ src/CHP1.Series/services/mock.repo.test.ts
@@ -3,8 +3,8 @@ import { getSeries } from './mock.repo';
 
 describe('Given mockRepo()', () => {
   describe('When we call it', () => {
-    test('Then it should return a list of series', () => {
-      const r = getSeries();
+    test('Then it should return a list of series', async () => {
+      const r = await getSeries();
       expect(r).toEqual(MOCK_SERIES);
     });
   });
