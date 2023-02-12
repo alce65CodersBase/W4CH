0cca9da39f0de46d8a0605732b418d721f752a4c
Alejandro Cerezo <alce65@hotmail.es>
2023-02-12 11:48:01 +0100

    ♻️ Refactor hook and its test

--- src/CH4.Phone/hooks/use.phone.test.tsx
+++ src/CH4.Phone/hooks/use.phone.test.tsx
@@ -1,17 +1,28 @@
-import { fireEvent, render, screen } from '@testing-library/react';
+import { act, fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { usePhone } from './use.phone';
 
+jest.useFakeTimers();
+jest.spyOn(global, 'setTimeout');
+
 describe('Given usePhone hook used in a component', () => {
+  const mockNumber = '666666666';
   function MockComponent() {
-    const { phoneNumber, isCalling, handleCall, handleHang } = usePhone();
+    const { isCalling, handleCall, handleHang, setPhoneNumber } = usePhone();
     return (
       <div>
-        <output>{phoneNumber}</output>
+        <output>PhoneNumber: {isCalling} </output>
         <output>State: {isCalling ? 'Calling' : 'Ready'}</output>
         <button
           onClick={() => {
-            handleCall(mockNumber);
+            setPhoneNumber(mockNumber);
+          }}
+        >
+          SetNumber
+        </button>
+        <button
+          onClick={() => {
+            handleCall();
           }}
         >
           Call
@@ -27,28 +38,58 @@ describe('Given usePhone hook used in a component', () => {
     );
   }
 
-  const mockNumber = '666666666';
-
   describe('When component it is rendered ', () => {
     let elements: HTMLElement[];
-    beforeEach(() => {
-      render(<MockComponent></MockComponent>);
+    beforeEach(async () => {
+      await act(async () => {
+        render(<MockComponent></MockComponent>);
+      });
+
       elements = [
         ...screen.getAllByRole('status'),
         ...screen.getAllByRole('button'),
       ];
     });
 
-    test('Then after click Call, phone number should be in the document', () => {
-      fireEvent.click(elements[2]);
+    test('Then after click Call, phone number should be in the document', async () => {
+      await act(async () => {
+        fireEvent.click(elements[2]); // Set Number
+      });
+      await act(async () => {
+        fireEvent.click(elements[3]); // Call
+      });
       expect(elements[0]).toHaveTextContent(mockNumber);
+      expect(elements[1]).toHaveTextContent('Calling');
     });
 
-    test('Then after click Hang, phone number should not be in the document', () => {
-      fireEvent.click(elements[2]);
+    test('Then after a Call, phone hang after 5 seconds', async () => {
+      await act(async () => {
+        fireEvent.click(elements[2]); // Set Number
+      });
+      await act(async () => {
+        fireEvent.click(elements[3]); // Call
+      });
+      await act(async () => {
+        jest.runAllTimers();
+      });
+      expect(setTimeout).toHaveBeenCalled();
+      expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 5000);
+    });
+
+    test('Then after click Hang, phone number should not be in the document', async () => {
+      await act(async () => {
+        fireEvent.click(elements[2]); // Set Number
+      });
+      await act(async () => {
+        fireEvent.click(elements[3]); // Call
+      });
       expect(elements[0]).toHaveTextContent(mockNumber);
-      fireEvent.click(elements[3]);
-      expect(elements[0]).toHaveTextContent('');
+      expect(elements[1]).toHaveTextContent('Calling');
+      await act(async () => {
+        fireEvent.click(elements[4]); // Hang
+      });
+      expect(elements[0]).not.toHaveTextContent(mockNumber);
+      expect(elements[1]).toHaveTextContent('Ready');
     });
   });
 });
--- src/CH4.Phone/hooks/use.phone.tsx
+++ src/CH4.Phone/hooks/use.phone.tsx
@@ -2,22 +2,25 @@ import { useState } from 'react';
 
 export type UsePhone = ReturnType<typeof usePhone>;
 export function usePhone() {
-  const [isCalling, setIsCalling] = useState(false);
+  const [isCalling, setIsCalling] = useState('');
   const [phoneNumber, setPhoneNumber] = useState('');
 
-  const handleCall = (phoneNumber: string) => {
-    setIsCalling(true);
-    setPhoneNumber(phoneNumber);
+  const handleCall = () => {
+    setIsCalling(phoneNumber);
+    setPhoneNumber('');
+    setTimeout(() => {
+      handleHang();
+    }, 5000);
   };
 
   const handleHang = () => {
-    setIsCalling(true);
-    setPhoneNumber('');
+    setIsCalling('');
   };
 
   return {
     isCalling,
     phoneNumber,
+    setPhoneNumber,
     handleCall,
     handleHang,
   };
