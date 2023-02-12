1ba0d71e5b9fd8b9bb35762383c7ecd39c8fdca9
Alejandro Cerezo <alce65@hotmail.es>
2023-02-12 11:48:36 +0100

    ✅ Update app test mocking all components

--- src/CH4.Phone/components/app/app.test.tsx
+++ src/CH4.Phone/components/app/app.test.tsx
@@ -1,13 +1,22 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { App } from './app';
+import { Actions } from '../actions/actions';
+import { Info } from '../info/info';
+import { Keyboard } from '../keyboard/keyboard';
 
+jest.mock('../actions/actions');
+jest.mock('../info/info');
+jest.mock('../keyboard/keyboard');
 describe('Given the component App', () => {
   describe('When it is rendered', () => {
     test('Then it should be on the screen', () => {
       render(<App />);
       const header = screen.getByRole('heading', { name: 'CH4 Phone' });
       expect(header).toBeInTheDocument();
+      expect(Info).toHaveBeenCalled();
+      expect(Keyboard).toHaveBeenCalled();
+      expect(Actions).toHaveBeenCalled();
     });
   });
 });
