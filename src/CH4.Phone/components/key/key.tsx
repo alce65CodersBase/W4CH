22b540650b9419431a02984e949dd970b087a5cb
Alejandro Cerezo <alce65@hotmail.es>
2023-02-12 11:47:11 +0100

    ✨ Add functionality with context

--- src/CH4.Phone/components/key/key.tsx
+++ src/CH4.Phone/components/key/key.tsx
@@ -1,12 +1,30 @@
+import { useContext } from 'react';
+import { AppContext } from '../../context/app.context';
+
 type KeyProps = {
   label: string;
   type?: string;
 };
 
 export function Key({ label, type = 'normal' }: KeyProps) {
+  const { phoneNumber, setPhoneNumber } = useContext(AppContext);
+
+  const handleClick = () => {
+    if (label === 'delete') {
+      setPhoneNumber('');
+      return;
+    }
+
+    if (phoneNumber.length < 9) {
+      setPhoneNumber(phoneNumber + label);
+    }
+  };
+
   return (
     <li>
-      <button className={'key ' + type}>{label}</button>
+      <button className={'key ' + type} onClick={handleClick}>
+        {label}
+      </button>
     </li>
   );
 }
--- src/CH4.Phone/components/keyboard/keyboard.test.tsx
+++ src/CH4.Phone/components/keyboard/keyboard.test.tsx
@@ -1,6 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Keyboard } from './keyboard';
+import { Key } from '../key/key';
+
+jest.mock('../key/key');
 
 describe('Given the component Keyboard', () => {
   describe('When it is rendered', () => {
@@ -8,6 +11,7 @@ describe('Given the component Keyboard', () => {
       render(<Keyboard />);
       const element = screen.getByRole('region', { name: 'keyboard' });
       expect(element).toBeInTheDocument();
+      expect(Key).toBeCalledTimes(11);
     });
   });
 });
